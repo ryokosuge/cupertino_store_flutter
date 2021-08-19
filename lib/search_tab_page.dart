03a2aa5594058f64973821b8b6a166f62607e2f5
ryokosuge <ryo.kosuge@gmail.com>
2021-08-19 14:32:17 +0900

    feat:	7. Add product search

--- lib/search_tab_page.dart
+++ lib/search_tab_page.dart
@@ -1,4 +1,9 @@
+import 'package:cupertino_store_flutter/product_row_item.dart';
+import 'package:cupertino_store_flutter/search_bar.dart';
+import 'package:cupertino_store_flutter/state/app_state_model.dart';
+import 'package:cupertino_store_flutter/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 class SearchTabPage extends StatefulWidget {
   const SearchTabPage({Key? key}) : super(key: key);
@@ -8,14 +13,65 @@ class SearchTabPage extends StatefulWidget {
 }
 
 class _SearchTabPageState extends State<SearchTabPage> {
+  late final TextEditingController _controller;
+  late final FocusNode _focusNode;
+  String _terms = '';
+
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: SearchBar(
+        controller: _controller,
+        focusNode: _focusNode,
+      ),
+    );
+  }
+
+  void _onTextChanged() {
+    setState(() {
+      _terms = _controller.text;
+    });
+  }
+
+  @override
+  void initState() {
+    _controller = TextEditingController()..addListener(_onTextChanged);
+    _focusNode = FocusNode();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
-        )
-      ],
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Styles.scaffoldBackground,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            _buildSearchBox(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: results.length,
+                itemBuilder: (context, index) => ProductRowItem(
+                  product: results[index],
+                  lastItem: index == results.length - 1,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
   }
 }
